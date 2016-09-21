5feabafa33ced825ad4a022df793b5e563c5c52a
Krozz Wu <krozzwu@gmail.com>
2016-09-20 23:00:46 -0400

    master gain

--- core/component/audio/oscillator/oscillator.js
+++ core/component/audio/oscillator/oscillator.js
@@ -4,6 +4,19 @@ console.log('init oscillator');
 // init audio context
 var audioContext = new AudioContext();
 
+var MasterGain = (function () {
+    var volumeInput = document.querySelectorAll('[data-knob="volume"]')[0];
+    var volumeValue = document.querySelectorAll('[data-knob="value"]')[0];
+    var gainNode = this;
+    this.gainNode = audioContext.createGain();
+    this.gainNode.gain.value = (volumeInput.value)/127;
+    volumeInput.addEventListener('change', function () {
+        gainNode.gainNode.gain.value = (this.value)/127;
+        volumeValue.innerHTML = (this.value)/127;
+    });
+});
+
+
 var Oscillator = (function (config) {
 
     if (config !== undefined) {
@@ -16,18 +29,25 @@ var Oscillator = (function (config) {
     this.oscNode.frequency.value = this.oscConfig.freq;
     this.oscGain.gain.value = '0';
     this.oscNode.connect(this.oscGain);
-    this.oscGain.connect(audioContext.destination);
+    // this.oscGain.connect(audioContext.destination);
+    var outputGain = new  MasterGain();
+    console.log(outputGain.gainNode.gain.value);
+    this.oscGain.connect(outputGain.gainNode);
+    outputGain.gainNode.connect(audioContext.destination);
+    // masterGain().connect(audioContext.destination);
     this.oscNode.start();
 
     this.noteOn = function () {
-        console.log('osc start');
-        this.oscGain.gain.value = '0.1';
+        this.oscGain.gain.value = outputGain.gainNode.gain.value;
     };
 
     this.noteOff = function () {
         this.oscGain.gain.value = '0';
     };
 });
+
+
+
 
 
 
